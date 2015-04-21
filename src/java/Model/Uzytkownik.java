1ea2a57ebac908087b337f0d0c711e4dcc0cdb42
Asiaaa <joanna.wolna@ymail.com>
2015-04-21 11:26:31 +0200

    Start

---
+++ src/java/Model/Uzytkownik.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Model;
+
+/**
+ *
+ * @author Joanna
+ */
+public class Uzytkownik {
+    public int id_uzytkownik;
+    private String imie;
+    private String nazwisko;
+    private String email;
+    
+}
